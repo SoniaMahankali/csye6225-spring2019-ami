f85cdeca47cb3b4639ac775b0a815d7dfb219576
Zhe <liu.zhe3@husky.neu.edu>
2019-03-14 18:49:45 -0400

    final-fix

--- csye6225-aws-cf-create-ci-cd-stack.sh
+++ csye6225-aws-cf-create-ci-cd-stack.sh
@@ -20,9 +20,9 @@ else
 fi
 
 # Domain name for ARN
-DOMAIN_NAME=$(aws route53 list-hosted-zones --query HostedZones[0].Name --output text)
-
-CD_DOMAIN="code-deploy."${DOMAIN_NAME%?}
+echo "Please enter your domain name"
+read domain
+CD_DOMAIN=code-deploy.$domain
 
 
 # Account id for arn
